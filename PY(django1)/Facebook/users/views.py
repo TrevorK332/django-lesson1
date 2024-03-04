e714efb61b95cea985a427d10ab762ed11ae3105
TrevorK332 <158285029+TrevorK332@users.noreply.github.com>
2024-03-04 10:39:41 +0300

    Add files via upload

---
+++ PY(django1)/Facebook/users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def contact(request):
+    return render(request, 'contact.html')
+def aboutus(request):
+    return render(request, 'aboutus.html')
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
